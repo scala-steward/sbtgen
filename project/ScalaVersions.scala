854492bf2df289240f2beeaf42e212cbb913b9fb
Kai <450507+neko-kai@users.noreply.github.com>
2024-05-13 13:37:56 +0100

    Update to Scala.js 1.16.0

--- project/ScalaVersions.scala
+++ project/ScalaVersions.scala
@@ -3,6 +3,6 @@ object ScalaVersions {
   val scala_213 = "2.13.14"
   val scala_3 = "3.2.2"
 
-  val scalaJsVersion = "1.15.0"
+  val scalaJsVersion = "1.16.0"
   val scalaNativeVersion = "0.5.1"
 }
