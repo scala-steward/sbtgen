a621533a3ac0c230f9262c93830e76ecdcf0fef6
Pavel Shirshov <pshirshov@eml.cc>
2025-05-22 21:14:08 +0100

    flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "SICK build environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs.buildPackages; [
+              ncurses
+              graalvm-ce
+              sbt
+
+              git
+              openssl
+            ];
+
+          };
+        }
+      );
+}
