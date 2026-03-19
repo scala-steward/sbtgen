2daf2ef096ff65912be34d33c85b4677634023b1
Kai <450507+neko-kai@users.noreply.github.com>
2026-03-19 19:44:55 +0000

    Reimplement IzumiGitStampPlugin via sbt-git to support worktrees

--- sbt/sbt-izumi/src/main/scala/izumi/sbt/plugins/IzumiGitStampPlugin.scala
+++ sbt/sbt-izumi/src/main/scala/izumi/sbt/plugins/IzumiGitStampPlugin.scala
@@ -1,50 +1,29 @@
 package izumi.sbt.plugins
 
-// copypasted https://bitbucket.org/atlassianlabs/sbt-git-stamp from due to obsolete jgit dependency
-
-import org.eclipse.jgit.api.Git
-import org.eclipse.jgit.lib._
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder
-import sbt.Keys._
-import sbt.{Def, _}
+import com.github.sbt.git.GitPlugin
+import com.github.sbt.git.SbtGit.GitKeys.*
+import sbt.Keys.*
+import sbt.{Def, *}
 import sbt.internal.util.ConsoleLogger
 
 object IzumiGitStampPlugin extends AutoPlugin {
   protected val logger: ConsoleLogger = ConsoleLogger()
 
+  override def requires: Plugins = super.requires && GitPlugin
+
   object Keys {
-    lazy val izGitObject = settingKey[Git]("Git object")
-    lazy val izGitRepositoryObject = settingKey[Repository]("Git repository object")
     lazy val izGitRevision = settingKey[String]("Git revision")
     lazy val izGitBranch = settingKey[String]("Git branch")
     lazy val izGitIsClean = settingKey[Boolean]("Git working dir status")
   }
 
-  import Keys._
+  import Keys.*
 
   override def globalSettings: Seq[Def.Setting[_]] = {
     Seq(
-      izGitObject := {
-        new Git(izGitRepositoryObject.value)
-      },
-      izGitRepositoryObject := {
-        val builder = new FileRepositoryBuilder
-        builder.readEnvironment.findGitDir.build
-      },
-      izGitRevision := {
-        val repository = izGitRepositoryObject.value
-        val head = repository.exactRef(Constants.HEAD)
-        ObjectId.toString(head.getObjectId)
-      },
-      izGitBranch := {
-        val repository = izGitRepositoryObject.value
-        repository.getBranch
-      },
-      izGitIsClean := {
-        val git = izGitObject.value
-        val status = git.status.call
-        status.isClean
-      },
+      izGitRevision := gitHeadCommit.value.getOrElse(""),
+      izGitBranch := gitCurrentBranch.value,
+      izGitIsClean := !(ThisBuild / gitUncommittedChanges).value,
       packageOptions += Def.task {
         val gitValues = Map(
           IzumiManifest.GitBranch -> izGitBranch.value,
@@ -57,7 +36,7 @@ object IzumiGitStampPlugin extends AutoPlugin {
             logger.debug(s"Manifest value: $k = $v")
         }
 
-        Package.ManifestAttributes(gitValues.toSeq: _*)
+        Package.ManifestAttributes(gitValues.toSeq*)
       }.value,
     )
   }
